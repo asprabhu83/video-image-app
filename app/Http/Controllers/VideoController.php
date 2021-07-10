57c958e0177df5aa7f63539172cde22c8527f133
Pradeep <a.sambandam.pradeep@gmail.com>
2021-07-10 09:36:36 +0530

    Added Models to VIdeo

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreVideoRequest;
 use App\Jobs\ConvertVideoForStreaming;
-use App\Video;
+use App\Models\Video;
 use Illuminate\Http\Request;
 
 class VideoController extends Controller
