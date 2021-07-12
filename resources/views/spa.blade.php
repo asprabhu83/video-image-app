44e1fdca47ecab3cc3391a9702481cdb6880289b
Prabhu <a.sambandam.prabhu@gmail.com>
2021-07-12 07:56:51 +0530

    Commited for including index page in

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -19,7 +19,7 @@
 <meta name="msapplication-TileColor" content="#000000"></head><body><noscript>
 <strong>We're sorry but analyzevideo doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
 </noscript><div id="app"></div>
-<script src="{{ mix('js/chunk-vendors.e222ddce.js') }}"></script>
-<script src="{{ mix('js/app.73f2cce8.js') }}"></script>
+<script src="../js/chunk-vendors.e222ddce.js"></script>
+<script src="../js/app.73f2cce8.js"></script>
 </body>
 </html>
