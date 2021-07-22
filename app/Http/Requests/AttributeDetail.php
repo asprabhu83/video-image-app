d65b965ebaa7d7215d5978469b79b1456ecec9d4
Prabhu <a.sambandam.prabhu@gmail.com>
2021-07-22 13:36:12 +0530

    Commited for including save

--- app/Http/Requests/AttributeDetail.php
+++ app/Http/Requests/AttributeDetail.php
@@ -13,7 +13,7 @@ class AttributeDetail extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/ClassDetail.php
+++ app/Http/Requests/ClassDetail.php
@@ -13,7 +13,7 @@ class ClassDetail extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
