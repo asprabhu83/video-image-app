57b2162cef3f6379c3f77158984833b26e93f528
ranjithgee <ranjithgee968@gmail.com>
2021-08-03 14:39:38 +0530

    change in permission table seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -25,6 +25,10 @@ class UserSeeder extends Seeder
         $permissions = [
             ['id' => 1, 'permission_name' => 'create project', 'description' => 'user have permission to create the files'],
             ['id' => 2, 'permission_name' => 'edit project', 'description' => 'user have permission to edit the files'],
+            ['id' => 3, 'permission_name' => 'delete project', 'description' => 'user have permission to delete the files'],
+            ['id' => 4, 'permission_name' => 'upload videos', 'description' => 'user have permission to upload the videos'],
+            ['id' => 5, 'permission_name' => 'export JSON', 'description' => 'user have permission to export the JSON files'],
+            ['id' => 6, 'permission_name' => 'manage users', 'description' => 'user have permission to manage account user']
         ];
 
         DB::table('permissions')->insert($permissions);
